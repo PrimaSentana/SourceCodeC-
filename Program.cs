037d7dc573f540f3c6cfc66ff77c445820490ef6
PrimaSentana <88036062+PrimaSentana@users.noreply.github.com>
2022-06-16 15:42:04 +0800

    Add files via upload
    Kalkulator Sederhana

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ----------Program Matematika C#---------- ");
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Pilih metode : ");
+            Console.WriteLine("1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.Write("-------------:"); string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.Write("Masukkan Nilai X : "); int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Masukkan Nilai Y : "); int y = Convert.ToInt32(Console.ReadLine());
+                int jumlah = x + y;
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Hasil = " + jumlah);
+            }
+            else if (choice == "2")
+            {
+                Console.Write("Masukkan Nilai X : "); int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Masukkan Nilai Y : "); int y = Convert.ToInt32(Console.ReadLine());
+                int jumlah = x - y;
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Hasil = " + jumlah);
+            }
+            else
+            {
+                Console.WriteLine("Parameter Invalid");
+                Main(args);
+            }
+        }
+    }
+}
